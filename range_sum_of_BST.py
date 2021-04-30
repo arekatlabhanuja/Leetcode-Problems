0450e16b157a1acb226c7a0f150f52d2df390574
arekatlabhanuja <36080380+arekatlabhanuja@users.noreply.github.com>
2021-04-30 10:58:05 -0400

    Add files via upload

---
+++ range_sum_of_BST.py
@@ -0,0 +1,24 @@
+#Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+#Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+#Output: 32
+
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        ans = 0
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                if low <= node.val <= high:
+                    ans += node.val
+                if low < node.val:
+                    stack.append(node.left)
+                if node.val < high:
+                    stack.append(node.right)
+        return ans
