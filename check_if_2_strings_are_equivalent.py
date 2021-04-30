ec8809a2052bdb25df17d0e3b0141a895aa0d2a1
arekatlabhanuja <36080380+arekatlabhanuja@users.noreply.github.com>
2021-04-30 11:06:22 -0400

    Add files via upload

---
+++ check_if_2_strings_are_equivalent.py
@@ -0,0 +1,31 @@
+# Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+# A string is represented by an array if the array elements concatenated in order forms the string.
+
+#  Example 1:
+
+# Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+# Output: true
+# Explanation:
+# word1 represents string "ab" + "c" -> "abc"
+# word2 represents string "a" + "bc" -> "abc"
+# The strings are the same, so return true.
+
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+        arr=""
+        for i in word1:
+            arr+=i
+        arr1=""
+        for i in word2:
+            arr1+=i
+        if arr==arr1:
+            return True
+        else:
+            return False
+
