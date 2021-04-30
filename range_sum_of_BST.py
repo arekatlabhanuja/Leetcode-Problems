612cd19b16eddafc3762cff770853a0560abf484
arekatlabhanuja <36080380+arekatlabhanuja@users.noreply.github.com>
2021-04-30 12:10:12 -0400

    Update range_sum_of_BST.py

--- range_sum_of_BST.py
+++ range_sum_of_BST.py
@@ -1,3 +1,4 @@
+#Range Sum of BST
 #Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
 #Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
 #Output: 32
