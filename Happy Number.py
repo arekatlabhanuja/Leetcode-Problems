61c3510e51c513f8370cb34030e5e8ab0b613e3a
arekatlabhanuja <36080380+arekatlabhanuja@users.noreply.github.com>
2021-05-18 10:34:46 -0400

    Add files via upload

---
+++ Happy Number.py
@@ -0,0 +1,44 @@
+# 202. Happy Number
+# Write an algorithm to determine if a number n is happy.
+
+# A happy number is a number defined by the following process:
+
+# Starting with any positive integer, replace the number by the sum of the squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+# Return true if n is a happy number, and false if not.
+# Example 1:
+
+# Input: n = 19
+# Output: true
+# Explanation:
+# 12 + 92 = 82
+# 82 + 22 = 68
+# 62 + 82 = 100
+# 12 + 02 + 02 = 1
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        sum = 0
+        save_sum = []
+        while sum != 1:
+            sum = 0
+            
+            if int(n) in save_sum:
+                break
+            else:
+                save_sum.append(int(n))
+
+            for num in str(n):
+                sum += int(num) * int(num)
+
+            if sum == 1:
+                return True
+            else:
+                n = str(sum)
+                
+        return False
