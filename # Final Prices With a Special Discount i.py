c4e5cc5f6ff76c40fb9c9480602789385a92c439
arekatlabhanuja <36080380+arekatlabhanuja@users.noreply.github.com>
2021-06-08 10:21:45 -0400

    Update # Final Prices With a Special Discount i.py

--- # Final Prices With a Special Discount i.py
+++ # Final Prices With a Special Discount i.py
@@ -17,7 +17,7 @@ class Solution(object):
         """
         for i in range(len(prices)):
             for j in range(i+1,len(prices)):
-                if prices[i]>prices[j]:
+                if prices[i]>=prices[j]:
                     prices[i]=prices[i]-prices[j]
                     break
         return prices
